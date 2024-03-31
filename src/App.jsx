d53e765f842235fe443533430b56ed295c7a8e34
GuiDev45 <bortolettodev@gmail.com>
2024-03-31 20:15:49 -0300

    002 / Exercício - 008 - JSX Arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./global.css";
 import JsxExerc from "./002-react-para-iniciantes/007-jsx/index";
+import JsxArrayExerc from "./002-react-para-iniciantes/008-jsx-array/index";
 
 export default function App() {
   return (
     <div>
-      <h1>Lista do Exercícios</h1>
+      <h1>Lista de Exercícios</h1>
       <JsxExerc />
+      <br />
+      <JsxArrayExerc />
     </div>
   );
 }
